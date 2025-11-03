e4aeaf7b9cf410ae5cee54bc2e6f9f8700327f1c
Chesed <potatolegend200@gmail.com>
2025-11-03 14:45:57 +0530

    test base
    shut the fuck up monarch

--- michaeeeel/scripts/test.gd
+++ michaeeeel/scripts/test.gd
@@ -1,6 +1,25 @@
 extends TileMapLayer
 
 func _ready() -> void:
+	#show_tile_debug()
+	spawn_tiles()
+
+var use_arr: Array[Vector2i]:
+	get:
+		var arr: Array[Vector2i] = []
+		for i in 17: for j in 8:
+			arr.push_back(Vector2i(i,j))
+		return arr
+
+func spawn_tiles() -> void:
+	var tile_preload: PackedScene = preload("res://scenes/tile.tscn")
+	for tile: Vector2i in use_arr:
+		var tile_node: Node2D = tile_preload.instantiate()
+		add_child(tile_node)
+		tile_node.position = map_to_local(tile)
+
+func show_tile_debug() -> void:
+	print(get_used_cells())
 	for tile: Vector2i in get_used_cells():
 		var centering_cont := CenterContainer.new()
 		var label := RichTextLabel.new()
