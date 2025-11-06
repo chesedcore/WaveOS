43dc366b3ae911f506cb9247ebee784542a2456f
Chesed <potatolegend200@gmail.com>
2025-11-06 18:15:14 +0530

    don't create the torment nexus
    creating the torment nexus rn

---
+++ michaeeeel/scripts/program_res.gd
@@ -0,0 +1,13 @@
+class_name ProgramResource extends Resource
+
+signal visuals_changed
+
+@export var name: String:
+	set(val):
+		name = val
+		visuals_changed.emit()
+@export var icon: CompressedTexture2D:
+	set(val):
+		icon = val
+		visuals_changed.emit()
+@export_file("*.tscn") var program: String
--- michaeeeel/scripts/ui/program_icon.gd
+++ michaeeeel/scripts/ui/program_icon.gd
@@ -1,11 +1,30 @@
+@tool
 class_name ProgramIcon extends MarginContainer
 
+@export var program_res: ProgramResource:
+	set(val):
+		program_res = val
+		if not val: return
+		if Engine.is_editor_hint(): return
+		val.visuals_changed.connect(_update_appearance)
 @export var icon_rect: TextureRect
+@export var name_label: RichTextLabel
+@export_tool_button("Update Visuals") var update: Callable = _update_appearance
 
 func _ready() -> void:
+	wire_up_signals()
+	_update_appearance()
+
+func wire_up_signals() -> void:
 	icon_rect.gui_input.connect(_on_icon_gui_input)
 
 func _on_icon_gui_input(event: InputEvent) -> void:
+	if Engine.is_editor_hint(): return
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			Bus.request_open_from_icon.emit(self)
+
+func _update_appearance() -> void:
+	if not program_res: return
+	self.icon_rect.texture = program_res.icon
+	self.name_label.text = "[b]"+program_res.name
