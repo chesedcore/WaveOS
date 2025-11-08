4b5c92ab73438f374f20eeacff6e6b4b24db9aec
Chesed <potatolegend200@gmail.com>
2025-11-08 11:55:14 +0530

    fixed multiple app duping crash

--- michaeeeel/scripts/program_registry.gd
+++ michaeeeel/scripts/program_registry.gd
@@ -3,12 +3,14 @@ class_name RegistryEntry
 var linked_program: Program
 var linked_task: Task
 
+##create a new entry with the given program and task from the taskbar.
 static func from(program: Program, task: Task) -> RegistryEntry:
 	var entry := new()
 	entry.linked_program = program
 	entry.linked_task = task
 	return entry
 
+##destroy both nodes linked in this entry.
 func destroy() -> void:
 	assert(linked_program and linked_task, "Either the program or the task is invalid...")
 	linked_program.queue_free()
--- michaeeeel/scripts/screen_handler.gd
+++ michaeeeel/scripts/screen_handler.gd
@@ -18,34 +18,40 @@ func wire_up_signals() -> void:
 
 func _on_icon_open_request(program_icon: ProgramIcon) -> void:
 	
+	#get a duped res, a dupe is important here to not link the same programres
+	#with multiple different programs
+	var key_res: ProgramResource = program_icon.program_res.duplicate()
+	
 	#open up a program
 	print("Request to open program from icon: ", program_icon)
 	var program: Program = program_wrapper.instantiate()
-	program.program_res = program_icon.program_res
+	program.program_res = key_res
 	program_dock.add_child.call_deferred(program)
 	await program.ready
 	
 	#add the minigame to the program
-	var scene: Minigame = load(program_icon.program_res.program).instantiate()
+	var scene: Minigame = load(key_res.program).instantiate()
 	program.wrap.call_deferred(scene)
 	await scene.ready
-	print("Request served, opened path at ", program_icon.program_res.program)
+	print("Request served, opened path at ", key_res.program)
 	
 	#add the program's task to the taskbar
-	var task := bottom_bar.add_to_taskbar(program_icon.program_res)
+	var task := bottom_bar.add_to_taskbar(key_res)
 	print("Added that program to the taskbar.")
 	
 	#add both the program and the task to the registry
-	registry[program_icon.program_res] = RegistryEntry.from(program, task)
+	registry[key_res] = RegistryEntry.from(program, task)
 	print("Added both to the registry.")
 
 func _on_close_request(program_res: ProgramResource) -> void:
 	print("Request to close constructs associated with ",program_res, " received.")
 	assert(program_res in registry, "Key doesn't exist in the registry... what teh fuck?")
 	
+	#get data from the registry and delete that entry
 	var entry := registry[program_res]
 	registry.erase(program_res)
 	print("Obtained registry entry. Entry deleted.")
 	
+	#destroy both nodes linked inside the registry
 	entry.destroy()
 	print("Program closed.")
